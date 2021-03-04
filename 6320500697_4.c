95c47ab2a5fa70b0d75d9df0501ba3c6e2d1b2e0
b6320500697 <78530335+b6320500697@users.noreply.github.com>
2021-03-04 17:13:49 +0700

    ข้อ 4

--- 6320500697_4.c
+++ 6320500697_4.c
@@ -8,4 +8,21 @@ int main()
     int i=0,j=0,k=0;
     scanf("%[^\n]s",s);
     p=strtok(s," ");
+    while(p!=NULL)
+    {
+        for(k=0;k<11;k++)
+        {
+            if (strcmp(p,w[k]))
+            {
+                i++;
+            }
+        }
+        if(i==11||j==0)
+        {
+            printf ("%c",toupper(p[0]));
+            p = strtok(NULL," ");
+            j++,i=0;
+        }
+    }
+    return 0;
 }
