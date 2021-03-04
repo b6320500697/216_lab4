773f1a16a1388ba396db4f23e3ed7c12c0557687
b6320500697 <78530335+b6320500697@users.noreply.github.com>
2021-03-04 15:51:45 +0700

    medal

--- 6320500697_1.c
+++ 6320500697_1.c
@@ -1,7 +1,22 @@
 #include <stdio.h>
 int main ()
 {
-    int n;
-     scanf ("%d",&n);
-     printf ("Enter N:");
+    int n,i,k;
+    scanf ("%d",&n);
+    int number [n][3];
+    for (i=0;i<n;i++)
+    {
+        for (k=0;k<3;k++)
+        {
+            scanf ("%d",&number[i][k]);
+        }
+    }
+    for (i=0;i<n;i++)
+    {
+        for (k=0;k<3;k++)
+        {
+            printf ("%d",&number[i][k]);
+        }
+    }
+
 }
