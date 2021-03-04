2b703997b84e8fb596c98605d3776d05c54e0444
b6320500697 <78530335+b6320500697@users.noreply.github.com>
2021-03-04 17:07:34 +0700

    ข้อ4

--- 6320500697_4.c
+++ 6320500697_4.c
@@ -5,4 +5,7 @@ int main()
 {
     char s[1000] ,w[11][5]={"i","of","the","on","at","for","with","a","an","in","and"};
     char *p;
+    int i=0,j=0,k=0;
+    scanf("%[^\n]s",s);
+    p=strtok(s," ");
 }
