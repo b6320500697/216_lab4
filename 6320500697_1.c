d9d82ff852d78971aac4669d2fa4ec907fa51387
b6320500697 <78530335+b6320500697@users.noreply.github.com>
2021-03-04 16:43:04 +0700

    Update 6320500697_1.c

--- 6320500697_1.c
+++ 6320500697_1.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 int main ()
 {
-    int n,i,k;
+    int n,i,k,min=2000,max=0,min1=0,sum=0,max1=0;
     scanf ("%d",&n);
     int number [n][3];
     for (i=0;i<n;i++)
@@ -11,12 +11,13 @@ int main ()
             scanf ("%d",&number[i][k]);
         }
     }
-    for (i=0;i<n;i++)
+    for(i=0;i<n-2;i++)
     {
-        for (k=0;k<3;k++)
+        for (k=i;k<i+3;i++)
         {
-            printf ("%d",&number[i][k]);
+            sum+=(number[k][0]*4)+(number[k][1]*2)+(number[k][2]*1)
         }
     }
+
 
 }
