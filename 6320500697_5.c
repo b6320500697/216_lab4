fdf4b73bc14f3310c79a7ce0e0439fe3222ff0a0
b6320500697 <78530335+b6320500697@users.noreply.github.com>
2021-03-04 17:37:07 +0700

    ข้อ5

--- 6320500697_5.c
+++ 6320500697_5.c
@@ -1,7 +1,23 @@
 #include <stdio.h>
 int main ()
 {
-    int n;
+    char c[100];
+    int n,b[4],j,i,t;
     scanf ("%d",&n);
+    for(j=0;j<n;j++)
+    {
+        b[0]=1;
+        scanf("%s",c);
+        for(i=0;i<strlen(c);i++)
+        {
+            if(c[i]=='A')
+            {
+                t = b[1];
+                b[1] = b[2];
+                b[2] = t;
+            }
+        }
+
+    }
     return 0;
 }
